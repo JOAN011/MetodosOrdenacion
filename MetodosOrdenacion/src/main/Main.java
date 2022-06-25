9bce45e8f4690f3b971593e0863c995e73a56cf9
JOAN011 <101189275+JOAN011@users.noreply.github.com>
2022-06-25 14:40:38 -0400

    Add files via upload

---
+++ MetodosOrdenacion/src/main/Main.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+import controlador.generarDatos.enums.Apellido;
+import controlador.generarDatos.enums.Nombre;
+import controlador.generarDatos.generarPersona;
+
+/**
+ *
+ * @author kali
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //Nombre[] nombre = Nombre.values();
+        //Apellido[] apellido = Apellido.values();
+        //System.out.println(nombre[0] + "\n" + String.valueOf(nombre.length)+ "\n" +String.valueOf(apellido.length)+ "\n" +apellido[0]);
+        //String valores = e.name().substring(3);
+        generarPersona generar = new generarPersona();
+        
+    }
+
+}
